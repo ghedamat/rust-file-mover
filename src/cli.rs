e52f7cc14ee430d215f4b359477c50f1cc53d98c
Mattia Gheda <ghedamat@gmail.com>
2014-10-10 12:04:49 -0400

    split cli in separate module and add loop

---
+++ src/cli.rs
@@ -0,0 +1,51 @@
+extern crate std;
+extern crate term;
+
+enum Color {
+    Green,
+    Red,
+    Yellow,
+    Blank
+}
+
+fn _ask(question: &str, col: Color) -> String {
+    _say(question, col);
+    std::io::stdio::stdin().read_line().unwrap()
+}
+
+pub fn ask(question: &str) -> String {
+    _ask(question, Blank)
+}
+
+pub fn ask_red(question: &str) -> String {
+    _ask(question, Red)
+}
+
+fn _say (s: &str, col: Color) {
+    let mut t = term::stdout().unwrap();
+    match col {
+        Green => { t.fg(term::color::GREEN).unwrap(); }
+        Red => { t.fg(term::color::RED).unwrap(); }
+        Yellow => { t.fg(term::color::BRIGHT_YELLOW).unwrap(); }
+        Blank => {}
+    }
+    (writeln!(t, "{}", s)).unwrap();
+    t.reset().unwrap();
+}
+
+pub fn say (s: &str) {
+    _say(s, Blank)
+}
+
+pub fn say_red (s: &str) {
+    _say(s, Red)
+}
+
+pub fn say_green (s: &str) {
+    _say(s, Green)
+}
+
+pub fn say_yellow (s: &str) {
+    _say(s, Yellow)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -8,92 +8,14 @@ use std::io::fs;
 use std::io;
 use libc::funcs::c95::stdlib;
 
-enum Color {
-    Green,
-    Red,
-    Yellow,
-    Blank
-}
-
-macro_rules! say(
-    ($s:expr, $col:expr) => ({
-        let mut t = term::stdout().unwrap();
-        if $col == "green" {
-            t.fg(term::color::GREEN).unwrap();
-        } else if $col == "red" {
-            t.fg(term::color::RED).unwrap();
-        }
-        (writeln!(t, "{}", $s)).unwrap();
-        t.reset().unwrap();
-    });
-)
-
-macro_rules! green(
-    ($s:expr) => ({
-        say!($s, "green")
-    });
-)
-
-macro_rules! red(
-    ($s:expr) => ({
-        say!($s, "red")
-    });
-)
-
-macro_rules! ask(
-    ($question:expr) => ({
-        println!($question);
-        std::io::stdio::stdin().read_line().unwrap()
-    });
-)
-
-fn _ask(question: &str, col: Color) -> String {
-    _say(question, col);
-    std::io::stdio::stdin().read_line().unwrap()
-}
-
-fn ask(question: &str) -> String {
-    _ask(question, Blank)
-}
-
-fn ask_red(question: &str) -> String {
-    _ask(question, Red)
-}
-
-fn _say (s: &str, col: Color) {
-    let mut t = term::stdout().unwrap();
-    match col {
-        Green => { t.fg(term::color::GREEN).unwrap(); }
-        Red => { t.fg(term::color::RED).unwrap(); }
-        Yellow => { t.fg(term::color::BRIGHT_YELLOW).unwrap(); }
-        Blank => {}
-    }
-    (writeln!(t, "{}", s)).unwrap();
-    t.reset().unwrap();
-}
-
-fn say (s: &str) {
-    _say(s, Blank)
-}
-
-fn say_red (s: &str) {
-    _say(s, Red)
-}
-
-fn say_green (s: &str) {
-    _say(s, Green)
-}
-
-fn say_yellow (s: &str) {
-    _say(s, Yellow)
-}
+mod cli;
 
 fn main() {
     println!("Hello, world!");
     let path = Path::new("/home/tha/Dev/RUST/PRJ/rust-file-mover");
-    say_red("reading directory:");
+    cli::say_red("reading directory:");
     visit_dirs(&path, all_files);
-    say_red("bye");
+    cli::say_red("bye");
 }
 
 enum Action {
@@ -121,39 +43,52 @@ fn get_action(ans: &str) -> Action {
 }
 
 fn all_files(path: &Path) -> Option<()> {
-    say_yellow(path.as_str().unwrap());
-    let a = ask("[Skip | Trash | Inside-dir | Outside-dir | Movie | mUsic | Var | Quit ]");
-    let ans = a.as_slice().trim();
-    let res = get_action(ans);
-    match res {
-        Inside if path.is_dir() => {
-            say_green("entering dir");
-            visit_dirs(path, all_files);
+    loop {
+        cli::say_yellow(path.as_str().unwrap());
+        let a = if path.is_dir() {
+            cli::ask("[Skip | Trash | Inside-dir | Outside-dir | Movie | mUsic | Var | Quit ]")
+        } else {
+            cli::ask("[Skip | Trash | Outside-dir | Movie | mUsic | Var | Quit ]")
+        };
+        let ans = a.as_slice().trim();
+        let res = get_action(ans);
+        match res {
+            Inside if path.is_dir() => {
+                cli::say_green("entering dir");
+                visit_dirs(path, all_files);
+            }
+            Skip => {
+                cli::say("skipping");
+                break;
+            }
+            Trash => {
+                cli::say("moving to trash..");
+                break;
+            }
+            Movie => {
+                cli::say("moving to movies..");
+                break;
+            }
+            Music => {
+                cli::say("moving to music..");
+                break;
+            }
+            Var => {
+                cli::say("moving to var..");
+                break;
+            }
+            Quit => {
+                cli::say("bye bye");
+                unsafe { libc::exit(0 as libc::c_int); }
+            }
+            Up => {
+                cli::say_green("leaving dir");
+                return None;
+            }
+            _ => {
+                cli::say_red("option not valid");
+            }
         }
-        Skip => {
-            say("skipping");
-        }
-        Trash => {
-            say("moving to trash..");
-        }
-        Movie => {
-            say("moving to movies..");
-        }
-        Music => {
-            say("moving to music..");
-        }
-        Var => {
-            say("moving to var..");
-        }
-        Quit => {
-            say("bye bye");
-            unsafe { libc::exit(0 as libc::c_int); }
-        }
-        Up => {
-            say_green("leaving dir");
-            return None;
-        }
-        _ => { say_red("option not valid"); }
     }
     Some(())
 }
